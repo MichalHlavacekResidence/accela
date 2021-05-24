839ec514a140be60e239ea84aab01906f2eed488
MichalHlavacekResidence <WEBmaster22*>
2021-05-24 15:58:09 +0200

    0.0.0.0.1 new file brand

--- accela/Controllers/HomeController.cs
+++ accela/Controllers/HomeController.cs
@@ -28,6 +28,11 @@ namespace accela.Controllers
             return View();
         }
 
+        public IActionResult Brand()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
---
+++ accela/Models/User.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace accela.Models
+{
+    public class User : IdentityUser
+    {
+        private int _id;
+        private string _firstname;
+        private string _lastname;
+        private string _email;
+        private string _password;
+
+        public User()
+        {
+
+        }
+        public int ID{get { return _id; } set{ _id = value; } }
+        public string Firstname { get { return _firstname; } set { _firstname = value; } }
+        public string Lastname { get { return _lastname; } set { _lastname = value; } }
+        public override string Email { get { return _email; } set { _email = value; } }
+        public string Password { get { return _password; } set { _password = value; } }
+
+
+
+
+        /*public IActionResult Index()
+        {
+           // return View();
+        }*/
+    }
+}
